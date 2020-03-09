00c757192df096084c81340197b978fdab549e2f
Zhang Fei <zhangfei.eason@gmail.com>
2020-03-09 11:27:08 +0800

    test: add InterProcessMutex, InterProcessSemaphoreMutex test cases

--- distributed-lock/src/test/java/com/zhangfei/distributedlock/CuratorLockTest.java
+++ distributed-lock/src/test/java/com/zhangfei/distributedlock/CuratorLockTest.java
@@ -1,19 +1,23 @@
 package com.zhangfei.distributedlock;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.test.TestingCluster;
 import org.apache.curator.utils.CloseableUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 public class CuratorLockTest {
@@ -26,13 +30,13 @@ public class CuratorLockTest {
 
     private static final String LOCK_PATH = "/distributed-lock";
 
-    private TestingCluster cluster;
+    private static TestingCluster cluster;
 
     // FakeLimitedResource simulates some external resource that can only be access by one process at a time
     private static final FakeLimitedResource resource = new FakeLimitedResource();
 
-    @BeforeEach
-    public void init() throws Exception {
+    @BeforeAll
+    public static void init() throws Exception {
         cluster = new TestingCluster(3);
         cluster.start();
     }
@@ -48,10 +52,8 @@ public class CuratorLockTest {
         for (int i = 0; i < QTY; ++i) {
             final int index = i;
             service.submit(() -> {
-                CuratorFramework client = CuratorFrameworkFactory.newClient(cluster.getConnectString(), new ExponentialBackoffRetry(1000, 3));
+                CuratorFramework client = genStartedZKClient();
                 try {
-                    client.start();
-
                     ExampleClientThatLock example = new ExampleClientThatLock(client, LOCK_PATH, resource, "Client " + index);
                     for (int j = 0; j < REPETITIONS; ++j) {
                         example.doWork(5, TimeUnit.SECONDS);
@@ -69,8 +71,49 @@ public class CuratorLockTest {
         service.awaitTermination(5, TimeUnit.MINUTES);
     }
 
-    @AfterEach
-    public void after() {
+    @Test
+    public void tesInterProcessMutex() throws Exception {
+        CuratorFramework client = genStartedZKClient();
+        InterProcessMutex lock = new InterProcessMutex(client, LOCK_PATH);
+        CuratorFramework client2 = genStartedZKClient();
+        InterProcessMutex lock2 = new InterProcessMutex(client2, LOCK_PATH);
+
+        assertTrue(lock.acquire(2L, TimeUnit.SECONDS));
+        assertTrue(lock.acquire(2L, TimeUnit.SECONDS));
+        assertFalse(lock2.acquire(2L, TimeUnit.SECONDS));
+        lock.release();
+        lock.release();
+        assertTrue(lock2.acquire(2L, TimeUnit.SECONDS));
+        lock2.release();
+        CloseableUtils.closeQuietly(client);
+        CloseableUtils.closeQuietly(client2);
+    }
+
+    @Test
+    public void testInterProcessSemaphoreMutex() throws Exception {
+        CuratorFramework client = genStartedZKClient();
+        InterProcessSemaphoreMutex lock = new InterProcessSemaphoreMutex(client, LOCK_PATH);
+        CuratorFramework client2 = genStartedZKClient();
+        InterProcessSemaphoreMutex lock2 = new InterProcessSemaphoreMutex(client2, LOCK_PATH);
+
+        assertTrue(lock.acquire(2L, TimeUnit.SECONDS));
+        assertFalse(lock.acquire(2L, TimeUnit.SECONDS));
+        assertFalse(lock2.acquire(2L, TimeUnit.SECONDS));
+        lock.release();
+        assertTrue(lock2.acquire(2L, TimeUnit.SECONDS));
+        lock2.release();
+        CloseableUtils.closeQuietly(client);
+        CloseableUtils.closeQuietly(client2);
+    }
+
+    @AfterAll
+    public static void after() {
         CloseableUtils.closeQuietly(cluster);
+    }
+
+    private CuratorFramework genStartedZKClient() {
+        CuratorFramework client = CuratorFrameworkFactory.newClient(cluster.getConnectString(), new ExponentialBackoffRetry(1000, 3));
+        client.start();
+        return client;
     }
 }
